a27d6318ad45aa8fc390fa8ef1a74e6a2e4fdb35
Aanchal2302 <aplpandey01@gmail.com>
2024-07-03 00:09:18 +0530

    Basics of React

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
-const heading =  React.createElement("div" , {id:"parent"},[React.createElement("h1", {id:"heading"}, "Hello world from react"), React.createElement("h2", {id:"heading"}, "Hello world from react h2")]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+// React Element
+const heading = <h1>Namaste React</h1>
+
+// Functional Component
+const HeadingComponent = () => {
+    return <h1 id="test">Namaste React from JSX</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+const component = ReactDOM.createRoot(document.getElementById("component"));
+component.render(<HeadingComponent/>);
